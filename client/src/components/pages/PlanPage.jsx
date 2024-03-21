34d43a0c41ac722f55358702910f0da7453dee55
kendlbat <kendlbat@edu.htl-villach.at>
2024-03-21 21:31:26 +0100

    Sie können auf eigene kosten heimfahrn

--- client/src/components/pages/PlanPage.jsx
+++ client/src/components/pages/PlanPage.jsx
@@ -1,9 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
 import { getFaecher } from "src/lib/api.mjs";
 
 export default function PlanPage() {
-    useEffect(() => {
-        getFaecher().then(console.log);
-    }, []);
+    useQuery({
+        queryKey: ["faecher"],
+        queryFn: () => {
+            getFaecher().then(console.log);
+        },
+    });
     return <></>;
 }
