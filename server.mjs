19a443fde6068d56e8251560971f0f83006b85c0
Maggusss <markus.schertler@edu.htl-villach.at>
2024-03-21 10:25:49 +0100

    db connection im backend; hat halt noch init stuff der ersetzt werden muss

--- server.mjs
+++ server.mjs
@@ -1,15 +1,51 @@
 import express from "express";
 import path from "node:path";
-const app = express();
 
+import { errorHandler }    from "./errors/error-handler.js";
+import { connectToDB, getReadyState } from "./db/db-manager.js";
 import apiRouter from "./api/index.mjs";
 
-// Static file host
-app.use(express.static(path.join(".", "client", "dist")));
-app.use(express.json());
+const PORT = process.env.PORT || 3000;
+const HOSTNAME = process.env.HOSTNAME || "0.0.0.0";
 
-app.use("/api", apiRouter);
+process.env.PGDATABASE = "schaepplidb";
+process.env.PGUSER = "schaeppli";
+process.env.PGPASSWORD = "tiajeiY6saeX9faaleexe0eeQuitahl4UShie5Aiqua4ahgei1eitiNgiejai7TohlaWa1baime";
+process.env.PGPORT = 5432;
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+async function main() {
+    console.log('starting server ...');
+
+    const app = express();
+
+    app.use(express.json());
+    app.use(express.text());
+
+    app.use((req,res,next) => {
+        console.log(req.url);
+        next();
+    });
+    app.use('/api/readyz', getReadyState);
+
+    // Static file host
+    app.use(express.static(path.join(".", "client", "dist")));
+    app.use(express.json());
+
+    app.use("/api", apiRouter);
+
+
+    app.use(errorHandler);
+
+    await connectToDB();
+
+    app.listen(PORT, HOSTNAME, () => {
+        console.log(`Server running on http://${HOSTNAME}:${PORT}`);
+        app.emit('server-up-and-running');
+    });
+}
+
+main().catch(err => {
+    console.log(`ExpressApp start failed. Errordetails here:`);
+    console.error(err);
+    process.exit(0);
 });
